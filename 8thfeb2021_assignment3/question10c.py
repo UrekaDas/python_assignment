d1fd5c6e2e8ba1c19284ad63f634274c85dda7e9
UrekaDas <77890483+UrekaDas@users.noreply.github.com>
2021-02-18 11:19:25 +0530

    Update question10c.py

--- 8thfeb2021_assignment3/question10c.py
+++ 8thfeb2021_assignment3/question10c.py
@@ -1,13 +1,2 @@
-n=int(input("enter a number: "))
-m=n
-i=1
-for i in range (1,i<=n):
-    i+=1
-    for j in range(1,j<i):
-        j+=1
-    print(" ",end="")
-    for k in range(1,k<=m):
-        k+=1
-    print("*",end="")
-    m-=1
-print("\n")
+for value in range(1,20):
+    print("*" * (20-value))
